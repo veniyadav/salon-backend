ec8124f51ac6aa0d730e99d89a621b18af252540
Veni Prasad Yadav <120805086+veniyadav@users.noreply.github.com>
2025-05-16 11:35:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,33 @@
+// local db
+//const mysql = require('mysql2/promise');
+
+//const db = mysql.createPool({
+//    host: '127.0.0.1',
+//    port: 3306,
+//    user: 'root',
+//    password: '',
+//    database: 'salon',
+//    multipleStatements: true
+//});
+
+ 
+
+//console.warn('Connected');
+
+//module.exports = db;
+
+// live db
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    host: '127.0.0.1',
-    port: 3306,
+    host: 'tramway.proxy.rlwy.net',
+    port: 35974,
     user: 'root',
-    password: '',
-    database: 'salon',
+    password: 'txsVaTfZGzbRRauPseaSvuNuZfSsypKN',
+    database: 'railway',
     multipleStatements: true
 });
-
- 
 
 console.warn('Connected');
 
 module.exports = db;
-
-
